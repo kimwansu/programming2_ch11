3c49ad8efacebf68023e4367ae74b7542206b6ff
Kim Wansu <wantalia@gmail.com>
2015-09-11 23:31:49 +0900

    누락 파일 추

---
+++ ch11_10/ch11_10.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int gcd(int x, int y);
+int gcd2(int x, int y);
+void get_lcm_gcd(int x, int y, int *p_lcm, int *p_gcd);
+
+int main()
+{
+    int a = 20;
+    int b = 16;
+    int gcd;
+    int lcm;
+
+    get_lcm_gcd(a, b, &lcm, &gcd);
+
+    printf("%d와 %d의 최소공배수: %d\n", a, b, lcm);
+    printf("%d와 %d의 최대공약수: %d\n", a, b, gcd);
+
+    return 0;
+}
+
+void get_lcm_gcd(int x, int y, int *p_lcm, int *p_gcd)
+{
+    *p_gcd = gcd(x, y);
+    *p_lcm = lcm(x, y);
+}
+
+int gcd(int x, int y)
+{
+    if (y == 0) {
+        return x;
+    }
+    else {
+        return gcd(y, x % y);
+    }
+}
+
+int lcm(int x, int y)
+{
+    return (x * y) / gcd(x, y);
+}
---
+++ ch11_11/ch11_11.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void merge(int *A, int *B, int*C, int size);
+void print_array(int *arr, int size);
+
+int main()
+{
+    int a[4] = { 2, 5, 7, 8 };
+    int b[4] = { 1, 3, 4, 6 };
+    int c[8];
+
+    merge(a, b, c, 4);
+
+    printf("배열 A의 내용:");
+    print_array(a, 4);
+    
+    printf("배열 B의 내용:");
+    print_array(b, 4);
+
+    printf("합쳐진 배열 C의 내용:");
+    print_array(c, 8);
+
+    return 0;
+}
+
+void merge(int *A, int *B, int*C, int size)
+{
+    int i = 0;
+    int j = 0;
+
+    while (i < size && j < size) {
+        if (A[i] < B[j]) {
+            C[i + j] = A[i];
+            i++;
+        }
+        else {
+            C[i + j] = B[j];
+            j++;
+        }
+    }
+
+    while (i < size) {
+        C[i + j] = A[i];
+        i++;
+    }
+
+    while (j < size) {
+        C[i + j] = B[j];
+        j++;
+    }
+}
+
+void print_array(int *arr, int size)
+{
+    int i;
+    for (i = 0; i < size; i++) {
+        printf(" %d", arr[i]);
+    }
+    printf("\n");
+}
