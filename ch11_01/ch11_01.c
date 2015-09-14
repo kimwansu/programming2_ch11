779312e0d218d86e565869a9fbd6caa489a3dbb3
Kim Wansu <wantalia@gmail.com>
2015-09-14 15:21:37 +0900

    타입 캐스팅 변경

--- ch11_01/ch11_01.c
+++ ch11_01/ch11_01.c
@@ -3,7 +3,7 @@
 int main()
 {
     int x = 0x12345678;
-    unsigned char *xp = (char *)&x;
+    unsigned char *xp = (unsigned char *)&x;
     printf("바이트 순서: %x %x %x %x\n", xp[0], xp[1], xp[2], xp[3]);
 
     return 0;
