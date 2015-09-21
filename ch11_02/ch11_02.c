d43b4e221c186ae04cff7d47d79ac5e0894852ec
Kim Wansu <wantalia@gmail.com>
2015-09-21 12:35:00 +0900

    누락 파일 추가

---
+++ ch11_02/ch11_02.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+void get_sum_diff(int x, int y, int* p_sum, int* p_diff);
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int sum;
+    int diff;
+
+    get_sum_diff(a, b, &sum, &diff);
+    printf("%d+%d=%d\n", a, b, sum);
+    printf("%d-%d=%d\n", a, b, diff);
+
+    return 0;
+}
+
+void get_sum_diff(int x, int y, int* p_sum, int* p_diff)
+{
+    *p_sum = x + y;
+    *p_diff = x - y;
+}
+
+// 11-3
+// 2개의 정수의 합과 차를 동시에 반환하는 함수 작성
+// 포인터 매개변수 사용
---
+++ ch11_06/ch11_06.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+void array_copy(int* A, int* B, int size);
+
+int main()
+{
+    int A[5] = { 1, 2, 3, 4, 5 };
+    int B[5];
+    int i;
+
+    array_copy(A, B, 5);
+    for (i = 0; i < 5; i++) {
+        printf("A[%d] = %d, B[%d] = %d\n", i, A[i], i, B[i]);
+    }
+
+    return 0;
+}
+
+void array_copy(int* A, int* B, int size)
+{
+    int i;
+    for (i = 0; i < size; i++) {
+        B[i] = A[i];
+    }
+}
+
+// 11-6
+// 정수 배열 A[]를 다른 정수 배열 B[]에 복사하는 함수 작성 테스트
